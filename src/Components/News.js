ff8dfb4e90792508d99f9f1b3e4cf9674741385a
Rahul Kumar <rahulcsofficial@gmail.com>
2022-07-12 15:44:01 +0530

    Infinite scrolling added

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { PropTypes } from "prop-types";
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export class News extends Component {
     static defaultProps = {
-        country : 'in',
-        pageSize : 6 ,
-        category : "general"    
+        country: 'in',
+        pageSize: 6,
+        category: "general"
     }
     static propTypes = {
         country: PropTypes.string,
@@ -21,7 +23,8 @@ export class News extends Component {
         this.state = {
             articles: [],
             loading: false,
-            page: 1
+            page: 1 ,
+            totalResults :0
         }
     }
     async componentDidMount() {
@@ -65,24 +68,51 @@ export class News extends Component {
             })
         }
     }
+
+    fetchMoreData = async () => {
+      this.setState({page : this.state.page +1})
+      let apiUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=dc56f66e9bf44f1f91538b64c8b34c82&page=${this.state.page + 1}&category=${this.props.category}&pageSize=${this.props.pageSize}`
+            this.setState({ loading: true })
+            let data = await fetch(apiUrl)
+            let parsedData = await data.json()
+            console.log(parsedData)
+            this.setState({
+               totalResults : parsedData.totalResults,
+                articles: this.state.articles.concat(parsedData.articles),
+                loading: false
+            })
+      
+      }
+
     render() {
         return (
-            <div className="container my-3">
-                <h2 className='text-center' style={{margin : '35px'}}>SoftNews Top headline</h2>
+            < >
+                <h2 className='text-center' style={{ margin: '35px' }}>SoftNews Top headline</h2>
                 {this.state.loading && <Spinner />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return (<div className='col-md-4' key={element.url}>
-                            <NewsItem title={element.title} description={element.description} imageurl={element.urlToImage} newsurl={element.url} author={element.author} publishDate={element.publishedAt.slice(0,10)+" "+element.publishedAt.slice(11,16)} source ={element.source.name} />
-                        </div>)
-                    })}
-                </div>
-                <div className='container d-flex justify-content-between'>
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length!=this.state.totalResults}
+                    loader={<Spinner/>}
+                >
+                    <div className='container'>
+                    <div className="row">
+
+                        {/*!this.state.loading &&*/ this.state.articles.map((element) => {
+                            return <div className='col-md-4' key={element.url}>
+                                <NewsItem title={element.title} description={element.description} imageurl={element.urlToImage} newsurl={element.url} author={element.author} publishDate={element.publishedAt.slice(0, 10) + " " + element.publishedAt.slice(11, 16)} source={element.source.name} />
+                            </div>
+                        })}
+                    </div>
+                    </div>
+                </InfiniteScroll>
+               {/*infinite scroll added*/}
+                {/* <div className='container d-flex justify-content-between'>
                     <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.previousClickHandle}>&#8249; Previous</button>
                     <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.nextClickHandle}>Next &#8250;</button>
 
-                </div>
-            </div>
+                </div> */}
+            </>
         )
     }
 }
